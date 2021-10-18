7163e62fb855b963525935779e1f9b5d5ecd806b
Ninabaldo <nbaldorousseau@gmail.com>
2021-10-18 17:45:32 +0200

    destacados

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,3 +8,11 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("myDiv").style.display = "block";
 }
+
+
+var list = document.querySelectorAll("div[data-image]");
+
+for (var i = 0; i < list.length; i++) {
+    var url = list[i].getAttribute('data-image');
+    list[i].style.backgroundImage = "url('" + url + "')";
+}
